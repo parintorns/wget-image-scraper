1abb43ecd0ed7ecee182231c3f287c216a5b082d
Parintorn <parintorns@gmail.com>
2016-04-21 01:57:43 +0700

    Fix Timestamp Error

--- scraper.sh
+++ scraper.sh
@@ -2,7 +2,7 @@
 for line in `cat sites.txt`; do
   # replace http://
   stripped_url=`echo $line| cut -c8-`
-  target_folder="downloads/`echo $stripped_url|sed 's/\//_/g'`"
+  target_folder="downloads/`echo $stripped_url|sed 's/\/?/_/g'`"
 
   echo $stripped_url
   echo ""
@@ -15,11 +15,11 @@ for line in `cat sites.txt`; do
   mkdir -p $target_folder
   echo "> scraping $stripped_url"
   wget -e robots=off \
-    -H -nd -nc -np \
+    -H -nd -np \
     --recursive -p \
     --level=1 \
     --accept jpg,jpeg,png,gif \
-    --convert-links -N \
+    --convert-links \
     --limit-rate=200k \
     --wait 1.0 \
     -P $target_folder $stripped_url
